1d778c7a1644b9aba98034e7111afc0230b8ec16
SychO9 <sychocouldy@gmail.com>
2020-09-05 17:19:29 +0100

    Fixes #3

--- index.template.php
+++ index.template.php
@@ -695,7 +695,7 @@ function template_quickbuttons($list_items, $list_class = null, $output_method =
 			$html .= $li['content'];
 		else
 			$html .= '
-				<a' . (!empty($li['href']) ? " href='{$li['href']}'" : '') . (!empty($li['javascript']) ? $li['javascript'] : '') . '>
+				<a' . (!empty($li['href']) ? " href='{$li['href']}'" : '') . (!empty($li['javascript']) ? $li['javascript'] : '') . ' class="preview" title="'.$li['label'].'">
 					' . (!empty($li['icon']) ? "<span class='main_icons {$li['icon']}'></span>" : '') . (!empty($li['label']) ? "<span class='item-label'>{$li['label']}</span>" : '') . '
 				</a>';
 
